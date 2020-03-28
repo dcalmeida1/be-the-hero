5aeca2a5f599d0029ba125763f42f36028efa85d
ADC <adc@ADCs-Mac-mini.local>
2020-03-27 22:47:06 -0300

    newincident

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,6 +3,7 @@ import { BrowserRoute, Route, Switch, BrowserRouter } from 'react-router-dom';
 import Logon from './pages/Logon';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
@@ -11,6 +12,7 @@ export default function Routes() {
                 <Route path="/" exact component={Logon}></Route>
                 <Route path="/register" component={Register}></Route>
                 <Route path="/profile" component={Profile}></Route>
+                <Route path="/incidents/new" component={NewIncident}></Route>
             </Switch>
         </BrowserRouter>
     );
